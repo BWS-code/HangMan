5db0276252a61e5615b2fb6790be1a759c718426
Sergey Shirnin <66397172+BWS-code@users.noreply.github.com>
2021-12-13 20:59:23 +1100

    Stage 5/8 Simple Loop 8 trials

--- hangman.py
+++ hangman.py
@@ -6,12 +6,10 @@ words = ('python', 'java', 'kotlin', 'javascript')
 pick = choice(words)
 trials = set('helloworld!')
 guessed = set()
-while trials and set(pick) != guessed:
+while trials:
     print(''.join([letter if letter in guessed else '-' for letter in pick]))
     entry = input('Input a letter:')
-    (guessed.add(entry), print()) if entry in set(pick) - guessed \
-        else \
-    (trials.pop(), print('No improvements{slash_n}'.format(slash_n='\n' if trials else ''))) if entry in guessed \
-        else \
-    (trials.pop(), print('That letter doesn\'t appear in the word{slash_n}'.format(slash_n='\n' if trials else '')))
-print(f'{pick}\nYou guessed the word!\nYou survived!' if trials else 'You lost!')
+    (guessed.add(entry), print()) if entry in pick else print('That letter doesn\'t appear in the word\n')
+    trials.pop()
+print('''Thanks for playing!
+We'll see how well you did in the next stage''')
