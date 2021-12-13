904e9577950a565ee76d7fe04c86871c2fbcc06e
Sergey Shirnin <66397172+BWS-code@users.noreply.github.com>
2021-12-13 16:26:50 +1100

    Stage 7/8 Errors

--- hangman.py
+++ hangman.py
@@ -1,70 +1,36 @@
 from random import choice
 from string import ascii_lowercase
-import re
 
 
-def update_checks():
-    if guess not in checked:
-        checked.append(guess)
+def entry_fails():
+    if len(entry) != 1:
+        return 'You should input a single letter\n'
+    if entry not in ascii_lowercase:
+        return 'Please enter a lowercase English letter\n'
+    if entry in guessed:
+        return 'You\'ve already guessed this letter\n'
+    return False
 
 
-def pre_check_ok():
-    check1 = len(guess) != 1
-    check2 = guess not in ascii_lowercase
-    check3 = guess in checked
-    check_fails = (check1, check2, check3)
-    messages = ('You should input a single letter',
-                'Please enter a lowercase English letter',
-                'You\'ve already guessed this letter')
-    if any(check_fails):
-        for num, check in enumerate(check_fails):
-            if check:
-                print(messages[num])
-                break
-        return False
-    return True
+print(' '.join('hangman'.upper()), end='\n\n')
+words = ('python', 'java', 'kotlin', 'javascript')
+pick = choice(words)
+trials, guessed = set('helloworld!'), set()
 
-
-def hit_or_miss():
-    if guess in pick:
-        indexes = [index for index, element in enumerate(pick) if element == guess]
-        update_checks()
-        for i in indexes:
-            encoded[i] = guess
-    else:
-        print('That letter doesn\'t appear in the word')
-        trials.pop()
-        update_checks()
-
-
-def win_or_lose():
-    if '-' in encoded:
-        print('You lost!')
-    else:
-        print(''.join(pick))
-        print('You guessed the word!\nYou survived!')
-
-
-print('H A N G M A N')
-options = ('python', 'java', 'kotlin', 'javascript')
-
-
-while 555 < 666:
-    checked, pick = list(), list(choice(options))
-    encoded = ['-' for _ in pick]
-    trials = set('lovehate!')
-    play_or_quit = input('Type "play" to play the game, "exit" to quit:')
-    print()
-    if play_or_quit == 'exit':
-        break
-    if play_or_quit == 'play':
-        while encoded != pick:
-            if not trials:
-                break
+while trials and not guessed.issuperset(set(pick)):
+    print(''.join([letter if letter in guessed else '-' for letter in pick]))
+    entry = input('Input a letter:')
+    
+    if entry_fails():
+        print(entry_fails())
+    elif entry in pick:
+        guessed.add(entry)
+        if not guessed.issuperset(set(pick)):
             print()
-            print(''.join(encoded))
-            guess = input('Input a letter: ')
-            if pre_check_ok():
-                hit_or_miss()
-        win_or_lose()
-        print()
+    else:
+        if not entry_fails():
+            guessed.add(entry)
+        trials.pop()
+        print('That letter doesn\'t appear in the word{slash_n}'.format(slash_n='\n' if trials else ''))
+
+print(f'You guessed the word {pick}!\nYou survived!' if trials else 'You lost!')
